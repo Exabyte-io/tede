352553ee064f449916b457247d24eaad65a4c105
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-01-15 23:48:44 +0200

    update: add cypress

---
+++ src/js/cypress/BrowserFactory.ts
@@ -0,0 +1,25 @@
+/* eslint-disable class-methods-use-this */
+import { Browser, BrowserSettings } from "./Browser";
+
+declare global {
+    interface Window {
+        browser: Browser;
+    }
+}
+
+export default class BrowserFactory {
+    static setBrowserSettings(settings: BrowserSettings) {
+        window.browser = new Browser(settings);
+    }
+
+    static setBrowser(browser: Browser) {
+        window.browser = browser;
+    }
+
+    static getBrowser(): Browser {
+        if (!window.browser) {
+            throw new Error("No browser object found");
+        }
+        return window.browser as Browser;
+    }
+}
---
+++ src/js/cypress/Widget.ts
@@ -0,0 +1,62 @@
+import { Browser } from "./Browser";
+import BrowserFactory from "./BrowserFactory";
+
+const modalBackdrop = ".modal-backdrop.fade";
+
+export default class Widget {
+    selector: string;
+
+    browser: Browser;
+
+    constructor(selector: string) {
+        this.selector = selector;
+        this.browser = BrowserFactory.getBrowser();
+    }
+
+    waitForVisible() {
+        return this.browser.waitForVisible(this.selector);
+    }
+
+    waitForDisappear(timeout?: number) {
+        return this.browser.waitForDisappear(this.selector, timeout);
+    }
+
+    waitForLoaderToDisappear() {
+        return this.browser.waitForDisappear(this.getWrappedSelector("div.spinner"));
+    }
+
+    getWrappedSelector(selector: string, separator = " ") {
+        return `${this.selector}${separator}${selector}`;
+    }
+
+    /**
+     * @summary Wraps the selectors, including functions, passed at the top level of config object
+     */
+    getWrappedSelectors<T extends object>(config: T): T {
+        const entries = Object.keys(config).map((key) => {
+            // @ts-ignore
+            const value = config[key];
+            let newValue;
+            switch (typeof value) {
+                case "string":
+                    newValue = this.getWrappedSelector(value);
+                    break;
+                case "function":
+                    newValue = (...args: unknown[]) => this.getWrappedSelector(value(...args));
+                    break;
+                default:
+                    newValue = value;
+            }
+            return [key, newValue];
+        });
+        return Object.fromEntries(entries);
+    }
+
+    waitForModalBackdropDisappear() {
+        this.browser.waitForDisappear(modalBackdrop);
+    }
+
+    isVisible() {
+        return this.browser.isVisible(this.selector);
+    }
+}
