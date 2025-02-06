0340f783861d11bc5f12123764a2ae8f175551af
VsevolodX <vsevolodbiryukov@icloud.com>
2025-02-06 12:49:55 -0800

    update: fix test

--- tests/js/TestCaseHandler.test.ts
+++ tests/js/TestCaseHandler.test.ts
@@ -2,25 +2,23 @@ import { expect } from "chai";
 
 import { TestCaseHandler } from "../../src/js/utils/TestCaseHandler";
 
-const testCaseConfig = {
+const testCase = {
     feature_name: "feature1",
     key1: "value1",
 };
 
 const templateSchema = {};
 
-const templateContent = `
-This is key1 = \${key1}
-`;
+const templateContent = `This is key1 = \${key1}`;
 
 describe("TestCaseHandler test", () => {
     it("should replace key1 in templateContent with value1", () => {
-        const testCaseHandler = new TestCaseHandler(
-            testCaseConfig,
+        const testCaseHandler = new TestCaseHandler({
+            testCase,
             templateSchema,
             templateContent,
-        );
-        const result = testCaseHandler.getTestCaseContent();
+        });
+        const result = testCaseHandler.getFeatureContent();
         expect(result).to.be.equal(`This is key1 = value1`);
     });
 });
