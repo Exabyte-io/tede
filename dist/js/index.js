15c6c076fe925006c01a7e1dffbf4940e6cecb7a
VsevolodX <vsevolodbiryukov@icloud.com>
2025-02-06 20:45:20 -0800

    chore: commit dist

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TestCaseHandler = exports.generateTestFeaturesFromTestConfig = void 0;
+exports.TestCaseHandler = exports.generateFeatureFilesFromConfig = exports.generateTestFeatureContentsFromTestCases = void 0;
 var utils_1 = require("./utils");
-Object.defineProperty(exports, "generateTestFeaturesFromTestConfig", { enumerable: true, get: function () { return utils_1.generateTestFeaturesFromTestConfig; } });
+Object.defineProperty(exports, "generateTestFeatureContentsFromTestCases", { enumerable: true, get: function () { return utils_1.generateTestFeatureContentsFromTestCases; } });
+Object.defineProperty(exports, "generateFeatureFilesFromConfig", { enumerable: true, get: function () { return utils_1.generateFeatureFilesFromConfig; } });
 var TestCaseHandler_1 = require("./utils/TestCaseHandler");
 Object.defineProperty(exports, "TestCaseHandler", { enumerable: true, get: function () { return TestCaseHandler_1.TestCaseHandler; } });
--- dist/js/utils/index.js
+++ dist/js/utils/index.js
@@ -1,11 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateTestFeaturesFromTestConfig = void 0;
+exports.generateFeatureFilesFromConfig = exports.generateTestFeatureContentsFromTestCases = void 0;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const TestCaseHandler_1 = require("./TestCaseHandler");
-/**
- * Generates test features from a test configuration object
- */
-function generateTestFeaturesFromTestConfig(templateContent, testCaseSchema, testCaseConfigs) {
+function generateTestFeatureContentsFromTestCases(templateContent, testCaseSchema, testCaseConfigs) {
     return testCaseConfigs.map((testCaseConfig) => {
         if (!testCaseSchema.$id) {
             testCaseSchema.$id = `testCaseSchema + ${Math.random()}`;
@@ -22,4 +24,25 @@ function generateTestFeaturesFromTestConfig(templateContent, testCaseSchema, tes
         };
     });
 }
-exports.generateTestFeaturesFromTestConfig = generateTestFeaturesFromTestConfig;
+exports.generateTestFeatureContentsFromTestCases = generateTestFeatureContentsFromTestCases;
+function generateFeatureFilesFromConfig(testConfig, inputDir, outputDir) {
+    try {
+        const { testCaseSchema } = testConfig;
+        const templatePath = path_1.default.join(inputDir, testConfig.template_path);
+        const templateContent = fs_1.default.readFileSync(templatePath, "utf8");
+        const features = generateTestFeatureContentsFromTestCases(templateContent, testCaseSchema, testConfig.cases);
+        const featurePath = path_1.default.join(outputDir, testConfig.feature_path);
+        if (!fs_1.default.existsSync(featurePath)) {
+            fs_1.default.mkdirSync(featurePath, { recursive: true });
+        }
+        features.forEach(({ name, content }) => {
+            const outputPath = path_1.default.join(featurePath, `${name}.feature`);
+            fs_1.default.writeFileSync(outputPath, content);
+            console.log(`Generated: ${outputPath}`);
+        });
+    }
+    catch (error) {
+        console.error(`Error generating feature files: ${error}`);
+    }
+}
+exports.generateFeatureFilesFromConfig = generateFeatureFilesFromConfig;
