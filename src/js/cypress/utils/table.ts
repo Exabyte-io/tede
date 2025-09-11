7cd7ed488df2d2a4794451996000989e75bc64a7
VsevolodX <vsevolodbiryukov@icloud.com>
2025-09-11 10:33:08 -0700

    update: add contains regex

--- src/js/cypress/utils/table.ts
+++ src/js/cypress/utils/table.ts
@@ -71,7 +71,7 @@ function evalExpression(str: string) {
 export function parseValue<T = string>(str: string): T {
     // eslint-disable-next-line no-shadow, no-use-before-define, @typescript-eslint/no-use-before-define
     const config = REGEXES.find((config) => str.match(config.regex));
-    return (config ? config.func(str, config.regex, context) : str) as T;
+    return (config ? config.func(str, config.regex, {}) : str) as T;
 }
 
 /**
@@ -220,6 +220,17 @@ const REGEXES: Regex[] = [
             return parseValue(
                 str.replace(`$CACHE{${value}}`, getValue(getCacheValue(contextKey), property)),
             );
+        },
+    },
+    {
+        name: "CONTAINS_STRING",
+        regex: /^\$CONTAINS\{(.*)}/,
+        func: (str, regex) => {
+            const match = str.match(regex);
+            if (!match) {
+                return null;
+            }
+            return (str.indexOf(match[1]) !== -1) as unknown;
         },
     },
 ];
